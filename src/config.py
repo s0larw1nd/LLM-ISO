15500f06861997543c67d037edfc80eb3de50f6e
s0larw1nd <alixess13@gmail.com>
2025-07-21 00:05:49 +0300

    updated config

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import os
 
-DEFAULT_MODEL = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", "Qwen2.5-7B-Instruct-merged")
-DEFAULT_EMBEDDINGS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", "Qwen3-Embedding-0.6B")
-DEFAULT_DB_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "db", "chroma_db")
-DEFAULT_DOC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "documents")
-DEFAULT_DOC_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "documents", "doc.docx")
+__parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
+DEFAULT_MODEL = os.path.join(__parent_dir, "models", "Qwen2.5-7B-Instruct-merged")
+DEFAULT_EMBEDDINGS = os.path.join(__parent_dir, "models", "Qwen3-Embedding-0.6B")
+DEFAULT_DB_DIR = os.path.join(__parent_dir, "db", "chroma_db")
+DEFAULT_DOC_DIR = os.path.join(__parent_dir, "documents")
+DEFAULT_DOC_FILE = os.path.join(__parent_dir, "documents", "doc.docx")
